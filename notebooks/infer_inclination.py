fixing units

notebooks/infer_inclination.py
@@ -29,6 +29,11 @@ filename = os.path.split(filepath)[1]
 print(filepath, filename)
 cube = imagecube(filepath, clip=1.1, kelvin='RJ')
 
+if len(sys.argv) >= 3:
+    nmax = int(sys.argv[2])
+else:
+    nmax = None
+
 # Pre-compute the meshgrid and flatten them.
 
 X, Y = np.meshgrid(cube.xaxis, cube.yaxis)
@@ -38,7 +43,13 @@ y = np.array(cube.data.flatten(), dtype=np.float64)
 
 # Take only positive values so we can work with the log and those within 1".
 
-mask = (y > 0.0) & (np.hypot(X, Y) <= 1.0) & (np.random.rand(len(y)) <= 1.0)
+mask = (y > 0.0) & (np.hypot(X, Y) <= 1.0)
+
+if nmax is not None and mask.sum() > nmax:
+    rands = np.random.rand(mask.sum())
+    r = np.sort(rands)[nmax]
+    mask[mask] = rands < r
+
 X = X[mask]
 Y = Y[mask]
 y = np.log(y[mask])
@@ -72,8 +83,9 @@ with pm.Model() as model:
     # Parameters
     x0 = pm.Normal("x0", mu=0.0, sd=0.1)
     y0 = pm.Normal("y0", mu=0.0, sd=0.1)
-    inc = pm.Uniform("inc", lower=0.0, upper=0.5*np.pi)
-    pa_deg = pm.Uniform("pa_deg", lower=0.0, upper=2.0*np.pi)
+    inc_deg = pm.Uniform("inc_deg", lower=0.0, upper=90.0)
+    inc = inc_deg * np.pi / 180.0
+    pa_deg = pm.Uniform("pa_deg", lower=0.0, upper=360.0)
     pa = pa_deg * np.pi / 180.0
     mu = pm.Normal("mu", mu=np.mean(y), sd=100.0)
     slope = pm.Normal("slope", mu=0.0, sd=100.0)
@@ -115,11 +127,11 @@ with pm.Model() as model:
 
     def optimize_geom(map_soln):
         map_soln = xo.optimize(map_soln, vars=[x0, y0])
-        map_soln = xo.optimize(map_soln, vars=[inc])
+        map_soln = xo.optimize(map_soln, vars=[inc_deg])
         map_soln = xo.optimize(map_soln, vars=[pa_deg])
-        map_soln = xo.optimize(map_soln, vars=[inc])
+        map_soln = xo.optimize(map_soln, vars=[inc_deg])
         map_soln = xo.optimize(map_soln, vars=[x0, y0])
-        map_soln = xo.optimize(map_soln, vars=[inc, pa_deg, x0, y0])
+        map_soln = xo.optimize(map_soln, vars=[inc_deg, pa_deg, x0, y0])
         return map_soln
 
     # Optimize to find the MAP
@@ -139,7 +151,13 @@ with pm.Model() as model:
     plt.plot(x_plot, y3_plot, "g", label="gp model")
     plt.legend(fontsize=10)
     plt.title(filename)
-    plt.savefig(filename.replace('.fits', '.png'), bbox_inches="tight")
+    if nmax is None:
+        fn = filename.replace('.fits', '.png')
+    else:
+        fn = filename.replace('.fits', '.{0}.png'.format(nmax))
+    plt.savefig(fn, bbox_inches="tight")
+
+print(map_soln)
 
 # Run the sampler.
 np.random.seed(42)
@@ -151,4 +169,8 @@ with model:
 
 # Save the trace.
 samples = pm.trace_to_dataframe(trace)
-samples.to_pickle('%s' % (filename.replace('.fits', '.trace.dat')))
+if nmax is None:
+    fn = filename.replace('.fits', '.trace.dat')
+else:
+    fn = filename.replace('.fits', '.{0}.trace.dat'.format(nmax))
+samples.to_pickle(fn)